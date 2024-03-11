f2920fc12d9618e57045563917851f2e6a9f3108
mattcroat <mattcroat.dev@gmail.com>
2024-03-11 22:30:06 +0100

    feat: Animate HSL value

--- src/slides/textColor.svelte
+++ src/slides/textColor.svelte
@@ -7,6 +7,7 @@
 	const color = signal({ bg: '#171717', text: '#fff' })
 	const picker = signal({ x: 930, y: 320, scale: 0 })
 	const slider = signal({ x: 0, scale: 0 })
+	const hsl = signal({ opacity: 0, l: 68 })
 
 	const interpolateColor = d3.interpolate('#5affff', '#005858')
 	const colorRamp: string[] = []
@@ -27,7 +28,9 @@
 <Slide>
 	<Step on:in={async () => await content.to({ opacity: 1, y: 0 })} />
 	<Step on:in={async () => await color.to({ text: '#afafaf' })} />
-	<Step on:in={async () => await color.to({ bg: '#5affff' })} />
+	<Step
+		on:in={async () => await color.to({ text: '#eeeeee', bg: '#5affff' })}
+	/>
 	<Step
 		on:in={async () => {
 			await picker.to({ scale: 1 }, { duration: 600 })
@@ -38,9 +41,11 @@
 			await color.to({ text: '#5affff' }, { duration: 0 })
 			await picker.to({ scale: 0 }, { duration: 600 })
 			await slider.to({ scale: 1 })
+			await hsl.to({ opacity: 1 })
 			await all(
-				slider.to({ x: 100 }, { duration: 2000 }),
-				color.to({ text: '#005858' }, { duration: 2000 })
+				hsl.to({ l: 17 }, { duration: 4000 }),
+				slider.to({ x: 100 }, { duration: 4000 }),
+				color.to({ text: '#005858' }, { duration: 4000 })
 			)
 		}}
 	/>
@@ -72,8 +77,8 @@
 		</svg> -->
 
 		<svg
-			width="100"
-			height="100"
+			width="60"
+			height="60"
 			viewBox="0 0 512 512"
 			class="absolute"
 			style:scale={$picker.scale}
@@ -81,7 +86,7 @@
 		>
 			<path
 				d="M432.081 190.621l53.305-53.304c32.671-32.671 36.702-82.099 4.046-114.754l-.001-.001c-32.655-32.655-82.097-28.608-114.754 4.047l-53.304 53.304L304.16 62.7c-26.726-26.726-67.128 13.679-40.403 40.403l15.277 15.275c-77.89 77.885-93.102 93.088-191.926 191.912-33.533 33.533-27.587 78.622-47.833 112.04-8.719 14.393-17.943 20.481-29.155 31.692-13.352 13.354-13.513 35.043-.354 48.202l.002.001c13.159 13.159 34.848 12.999 48.201-.355 11.353-11.352 15.735-18.791 29.776-27.952 33.317-21.73 79.775-14.854 113.956-49.035 98.982-98.977 113.035-113.039 191.918-191.919l15.272 15.272c26.724 26.725 67.129-13.676 40.403-40.403l-17.213-17.212zM275.284 322.599c-44.743-10.923-96.383 10.319-136.526-35.253l154.623-154.623 85.889 85.89-103.986 103.986z"
-				fill="#001f1f"
+				fill="#005858"
 			/>
 		</svg>
 
@@ -94,6 +99,12 @@
 				style:left="calc({$slider.x}% - 10px)"
 			/>
 			<div class="h-[40px] rounded" style:background={gradient} />
+			<span
+				class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-mono text-[18px]"
+				style:opacity={$hsl.opacity}
+			>
+				hsl(180 100% {$hsl.l.toFixed()}%)
+			</span>
 		</div>
 
 		<div
