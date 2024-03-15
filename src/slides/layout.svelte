5869ddb99c3b85870621ff947a6978ba4f102ea5
mattcroat <mattcroat.dev@gmail.com>
2024-03-15 11:16:33 +0100

    chore: Update layout

--- src/slides/layout.svelte
+++ src/slides/layout.svelte
@@ -4,13 +4,20 @@
 	import { Slide, Step } from '@components'
 	import { signal } from '@motion'
 
-	let step: 'start' | 'title' | 'wireframe' = 'start'
+	let step:
+		| 'start'
+		| 'title'
+		| 'inspiration.layout'
+		| 'inspiration.design'
+		| 'wireframe' = 'start'
 
 	const wireframe = signal({ x: 5 })
 </script>
 
 <Slide>
 	<Step on:in={() => (step = 'title')} />
+	<Step on:in={() => (step = 'inspiration.layout')} />
+	<Step on:in={() => (step = 'inspiration.design')} />
 	<Step on:in={async () => (step = 'wireframe')} />
 	<Step
 		on:in={async () => await wireframe.to({ x: -90 }, { duration: 2000 })}
@@ -30,12 +37,35 @@
 		</div>
 	{/if}
 
+	{#if step === 'inspiration.layout'}
+		<div class="h-full grid place-content-center">
+			<img
+				transition:fly={{ y: 1000, duration: 1000 }}
+				class="translate-y-[74px]"
+				src="layout.png"
+				alt="inspiration"
+			/>
+		</div>
+	{/if}
+
+	{#if step === 'inspiration.design'}
+		<div class="h-full grid place-content-center">
+			<img
+				in:fly={{ y: 1000, duration: 1000, delay: 1000 }}
+				out:fly={{ y: 1000, duration: 1000 }}
+				class="translate-y-[74px]"
+				src="swiss.png"
+				alt="inspiration"
+			/>
+		</div>
+	{/if}
+
 	{#if step === 'wireframe'}
 		<div
 			transition:fly={{
 				x: 400,
 				duration: 1000,
-				delay: 300,
+				delay: 1000,
 				easing: cubicInOut,
 			}}
 			class="h-full flex gap-16"
